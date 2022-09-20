103d524a2500bb50d5a2119141baa9d7619ace6d
JohanOh06 <90780186+JohanOh06@users.noreply.github.com>
2022-09-20 09:30:00 -0500

    9/20/22
    almost done, just have to record

--- app/src/main/java/com/example/practiceapp/MainActivity.java
+++ app/src/main/java/com/example/practiceapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.practiceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void happy(View view){
+        TextView myTV = findViewById(R.id.textView3);
+        myTV.setText("your mood is happy!");
+    }
+    public void sad(View view){
+        TextView myTV = findViewById(R.id.textView3);
+        myTV.setText("your mood is sad");
+    }
+
+    public void tired(View view){
+        TextView myTV = findViewById(R.id.textView3);
+        myTV.setText("your mood is tired");
+    }
+
+    public void mad(View view){
+        TextView myTV = findViewById(R.id.textView3);
+        myTV.setText("your mood is mad");
+    }
+
 }
